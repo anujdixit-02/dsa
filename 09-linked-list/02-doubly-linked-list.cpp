cd6e0be5b85ae7161ee4036d1c0d911dea7f5e3e
Anuj Dixit <anuj29.2dixit@gmail.com>
2026-07-12 08:19:17 +0530

    linked list

--- 09-linked-list/02-doubly-linked-list.cpp
+++ 09-linked-list/02-doubly-linked-list.cpp
@@ -12,9 +12,76 @@ class node {
     this->next = NULL;
     this->previous = NULL;
   }
-  //destructor
+ 
   
 };
+
+void insertAtHead(node* &head, int data){
+  node* node1 = new node(data);
+  node1->next = head;
+  head->previous = node1;
+  head = node1;
+}
+
+void insertAtTail(node* &tail,int data){
+  node* temp = new node(data);
+  tail->next = temp;
+  temp->previous = tail;
+  tail = temp;
+
+}
+
+void insertAtPosition(node* &head, node* &tail,int position, int data){
+  if(position == 1){
+    insertAtHead(head,data);
+    return;
+  }
+
+    node* temp = head;
+    int count = 1;
+    while(count < position-1){
+      temp = temp->next;
+      count++;
+    }
+    node* node2 = new node(data);
+    node2->next = temp->next;
+    node2->previous = temp;
+    temp->next->previous = node2;
+    temp->next = node2;
+
+      if(temp ->next == NULL){
+    insertAtTail(tail,data);
+    return;
+  }
+  }
+
+  //deleting the node
+  void deleteNode(int position, node* &head){
+  //deleting start node
+  if(position == 1){
+    node* temp = head;
+    head = head->next;
+    if(head != NULL){
+    head->previous = NULL;
+    }
+    delete temp;
+  }
+  else{
+    node* current = head;
+    //node* previous = NULL;
+    int count = 1;
+    while(count < position){
+   // previous = current;
+    current = current->next;
+    count++; 
+    }
+    current ->previous->next = current->next;
+    if(current->next != NULL)
+    current->next->previous = current->previous;
+    delete current;
+  }
+}
+
 
 void printLinkedList(node* head){
   node* temp = head;
@@ -25,8 +92,28 @@ void printLinkedList(node* head){
   cout << endl;
 }
 
+int getLen(node* head){
+    node* temp = head;
+    int len = 0;
+  while(temp != NULL){
+    len++;
+    temp = temp->next;
+  }
+  return len;
+}
+
 int main(){
-  node* head = new node(10);
+  node* node1 = new node(10);
+  node* head = node1;
+  node* tail = node1;
+  insertAtHead(head,11);   
   printLinkedList(head);
+  insertAtTail(tail,2);
+  printLinkedList(head);
+deleteNode(2,head);
+printLinkedList(head);
+  insertAtPosition(head,tail,2,9);
+  printLinkedList(head);
+  cout << "Lenght of the list : " << getLen(head);
   return 0;
 }
