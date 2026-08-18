7cf0becd183d20e4e57018016ebeabcdfac8afb7
Anuj Dixit <anuj29.2dixit@gmail.com>
2026-08-19 01:13:32 +0530

    tree

--- 12-tree/08-boundary-traversaal.cpp
+++ 12-tree/08-boundary-traversaal.cpp
@@ -15,3 +15,107 @@ class node{
   }
 };
 
+
+void traveseLeft(node* root, vector<int> &ans){
+
+  //base case
+  if(root == NULL){
+  return;
+  }
+  if(root->left == NULL && root->right == NULL){
+    return;
+  }
+
+  ans.push_back(root->data);
+  if(root->left){
+    traveseLeft(root->left,ans);
+  }
+  
+  else{
+    traveseLeft(root->right,ans);
+  }
+}
+
+void traverseLeaf(node* root, vector<int> &ans){
+  //base case
+  if(root == NULL)
+  return;
+
+  if(root->left == NULL && root->right == NULL){
+    ans.push_back(root->data);
+    return;
+  }
+traverseLeaf(root->left,ans);
+traverseLeaf(root->right,ans);
+} 
+void traverseRight(node* root,vector<int> &ans){
+ if(root == NULL){
+  return;
+ }
+  if(root->left == NULL && root ->right == NULL){
+    return;
+  }
+
+  if(root->right){
+    traverseRight(root->right,ans);
+  }
+  else{traverseRight(root->left,ans);}
+
+  ans.push_back(root->data);
+
+}
+vector<int> boundary(node* root){
+  vector<int> ans;
+  if(root == NULL){
+  return ans;
+  }
+  ans.push_back(root->data);
+
+  //left part print ans store
+  traveseLeft(root->left,ans);
+
+  //traversal leaf node
+
+  //left
+  traverseLeaf(root->left,ans);
+
+  //right
+  traverseLeaf(root->right,ans);
+
+  //traversalright
+  traverseRight(root->right,ans);
+
+  return ans;
+}
+
+
+int main(){
+
+    node* root = new node(1);
+
+    root->left = new node(2);
+    root->right = new node(3);
+
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    root->left->right->left = new node(8);
+    root->left->right->right = new node(9);
+
+    root->right->right->right = new node(10);
+
+    vector<int> ans = boundary(root);
+
+    cout << "Boundary Traversal: ";
+
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
---
+++ 12-tree/09-vertical-order-traversal.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<map>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+vector<int> verticalOrder(node* root){
+
+    map<int, map<int, vector<int>>> nodes;
+    queue<pair<node*, pair<int,int>>> q;
+    vector<int> ans;
+
+    if(root == NULL){
+        return ans;
+    }
+
+    // root -> horizontal distance = 0, level = 0
+    q.push(make_pair(root, make_pair(0, 0)));
+
+    while(!q.empty()){
+
+        pair<node*, pair<int,int>> temp = q.front();
+        q.pop();
+
+        node* frontNode = temp.first;
+
+        int hd = temp.second.first;
+        int level = temp.second.second;
+
+        // Store node according to HD and level
+        nodes[hd][level].push_back(frontNode->data);
+
+        // Left child
+        if(frontNode->left != NULL){
+            q.push(make_pair(
+                frontNode->left,
+                make_pair(hd - 1, level + 1)
+            ));
+        }
+
+        // Right child
+        if(frontNode->right != NULL){
+            q.push(make_pair(
+                frontNode->right,
+                make_pair(hd + 1, level + 1)
+            ));
+        }
+    }
+
+    // Convert map into answer
+    for(auto i : nodes){
+
+        for(auto j : i.second){
+
+            for(auto k : j.second){
+
+                ans.push_back(k);
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+
+    node* root = new node(1);
+
+    root->left = new node(2);
+    root->right = new node(3);
+
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    vector<int> ans = verticalOrder(root);
+
+    cout << "Vertical Order Traversal: ";
+
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
